58584e75627863f8b0ca376ac24424545ec3ee88
Jimmy Tjotola <tjotolajimmy@gmail.com>
2024-12-15 16:56:45 -0800

    Two sum with hash map

--- DSA PRACTICE/Program.cs
+++ DSA PRACTICE/Program.cs
@@ -6,6 +6,9 @@ public class Output
 {
     public static void Main(String[] args)
     {
-        TwoSum.calculate(9);
+        //TwoSum.calculate(9);
+
+        int[] target = TwoSum.TwoSums(TwoSum.nums ,17);
+        Console.WriteLine($"Indices: [{target[0]}, {target[1]}]");
     }
 }
