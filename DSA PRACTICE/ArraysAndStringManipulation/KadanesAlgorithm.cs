562f154092e96fa8c2d18b05592372160586cef7
Jimmy Tjotola <tjotolajimmy@gmail.com>
2024-12-15 17:34:22 -0800

    KadananesAlgorithm for subarrays etc

---
+++ DSA PRACTICE/ArraysAndStringManipulation/KadanesAlgorithm.cs
@@ -0,0 +1,39 @@
+﻿namespace DSA_PRACTICE.ArraysAndStringManipulation
+{
+    /// <summary>
+    /// Efficent follows O(N)
+    /// </summary>
+    //Find the maximum sum of a contiguous subarray in a given array of integers(which may contain negative numbers).
+
+    //Input: An array nums of integers(both positive and negative).
+    //Output: The maximum possible sum of a contiguous subarray.
+    //Example 1:
+    //Input: nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    //Output: 6
+    //Explanation: The subarray [4, -1, 2, 1] has the largest sum = 6.
+    //Example 2:
+    //Input: nums = [1]
+    //Output: 1
+    //Explanation: The array only has one element, so the maximum subarray is the single element itself.
+    //Example 3:
+    //Input: nums = [5, 4, -1, 7, 8]
+    //Output: 23
+    //Explanation: The subarray [5, 4, -1, 7, 8] has the largest sum = 23.
+    public class KadanesAlgorithm
+    {
+        public static int[] Numbers = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+
+        public static int maxSubArray(int[] nums)
+        {
+            int current_sum = nums[0];
+            int max_sum = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                current_sum = Math.Max(nums[i],current_sum + nums[i]);
+                max_sum = Math.Max(max_sum, current_sum);
+            }
+            return max_sum;
+        }
+    }
+}
--- DSA PRACTICE/Program.cs
+++ DSA PRACTICE/Program.cs
@@ -6,9 +6,12 @@ public class Output
 {
     public static void Main(String[] args)
     {
-        //TwoSum.calculate(9);
+        /// TwoSum
+        //int[] target = TwoSum.TwoSums(TwoSum.nums ,17);
+        //Console.WriteLine($"Indices: [{target[0]}, {target[1]}]");
 
-        int[] target = TwoSum.TwoSums(TwoSum.nums ,17);
-        Console.WriteLine($"Indices: [{target[0]}, {target[1]}]");
+       ///KadansAlgorithm
+        int result = KadanesAlgorithm.maxSubArray(KadanesAlgorithm.Numbers);
+        Console.WriteLine($"Output{result}");
     }
 }
